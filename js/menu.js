b695b55b0bc1a41ed434d09cca9a2860ca6887b5
Gustavo Marques <gustavomna18@gmail.com>
2024-11-18 23:56:08 -0300

    Verificador-url

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,10 @@ function EMmenu() {
    var img = document.getElementById("img")
    var menu = document.getElementById("menu")
    if (img.src.includes("menu_white_36dp.svg")) {
-      img.src = "img/close_white_36dp.svg"
+      img.src = "/img/close_white_36dp.svg"
       menu.classList.remove("esconder")
    } else {
-      img.src = "img/menu_white_36dp.svg"
+      img.src = "/img/menu_white_36dp.svg"
       menu.classList.add("esconder")
    }
 }
